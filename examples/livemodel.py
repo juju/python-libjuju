3ad963c726e377cf812b8298df2ad1d5f68d672d
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2016-10-07 12:11:06 -0400

    Fix livemodel example

--- examples/livemodel.py
+++ examples/livemodel.py
@@ -1,22 +1,30 @@
+"""
+This example:
+
+1. Connects to the current model
+2. Watches the model and prints all changes
+3. Runs forever (kill with Ctrl-C)
+
+"""
 import asyncio
 
 from juju.model import Model
-from juju.client.connection import Connection
 
 
-loop = asyncio.get_event_loop()
-conn = loop.run_until_complete(Connection.connect_current())
-
-
-def on_model_change(delta, old, new, model):
+async def on_model_change(delta, old, new, model):
     print(delta.entity, delta.type, delta.data)
     print(old)
     print(new)
     print(model)
 
-async def watch_model():
-    model = Model(conn)
-    model.add_observer(on_model_change)
-    await model.watch()
 
-loop.run_until_complete(watch_model())
+async def watch_model():
+    model = Model()
+    await model.connect_current()
+
+    model.add_observer(on_model_change)
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(watch_model())
+loop.run_forever()
