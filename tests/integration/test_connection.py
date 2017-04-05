f93420deb029b6938f73d28a4f7a414b2b9655dc
Pete Vander Giessen <petevg@gmail.com>
2017-04-05 17:46:24 -0400

    Added Monitor class to Connection. (#105)
    Added Monitor class to Connection.
    
    Calling Monitor.status allows a client to determine whether the
    connection is "connected", "disconnected" or in an "error" state, and
    take appropriate action as a result.
    
    This is meant to address the issue of watchers failing silently when the
    websocket connection goes away due to a network issue.

--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -12,3 +12,31 @@ async def test_connect_current(event_loop):
 
         assert isinstance(conn, Connection)
         await conn.close()
+
+
+@base.bootstrapped
+@pytest.mark.asyncio
+async def test_monitor(event_loop):
+
+    async with base.CleanModel():
+        conn = await Connection.connect_current()
+
+        assert conn.monitor.status == 'connected'
+        await conn.close()
+
+        assert conn.monitor.status == 'disconnected'
+
+
+@base.bootstrapped
+@pytest.mark.asyncio
+async def test_monitor_catches_error(event_loop):
+
+    async with base.CleanModel():
+        conn = await Connection.connect_current()
+
+        assert conn.monitor.status == 'connected'
+        await conn.ws.close()
+
+        assert conn.monitor.status == 'error'
+
+        await conn.close()
