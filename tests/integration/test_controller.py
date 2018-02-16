juju/python-libjuju

commit 978f35cc4f19ba98af86ca73a387c9997ad6b24c

refactor connections prior to bakery authentication (#187)
We do a bunch of refactoring in advance of actually adding
the bakery authentication. No additional functionality
as yet (hence no new tests) but this is a breaking API
change in a few ways.

API changes
----

Model and Controller now use the connect method for both connecting to a named model and for connecting to the current model (if the name is empty or None)

Connection.connect is now a class method that returns a new connected connection.

Various fields in Connection, Model and Controller that are really internal-only are given a leading underscore.

Controller and Model now have connection methods not properties, and they raise an exception if there is no current connection.

Other changes that should not break the API
----

Add a bakery_client argument to Connection.connect so that the client can have control over what interaction methods are used and provide a cookie jar.

Use "-q -B" options to scp so that it doesn't print spurious output when it's used.

Avoid unnecessary explicit inheritance from object.

Add is_connected method to Controller and Model.

Made new Connector class factoring out code in common between Controller and Model.

Factor Connection._connect so that we can do the bakery authentication.

Connection has a new connect_params method that returns parameters suitable for passing to Connection.connect.

Factored out commonality from Connection.pinger and Connection.receiver to _Task, making it easier to disable or mock those tasks in tests.

juju/client/connector.py
@@ -1,0 +1,133 @@
+import asyncio
+import logging
+
+import macaroonbakery.httpbakery as httpbakery
+from juju.client.connection import Connection
+from juju.client.jujudata import JujuData
+from juju.errors import JujuConnectionError, JujuError
+
+log = logging.getLogger('connector')
+
+
+class NoConnectionException(Exception):
+    '''Raised by Connector when the connection method is called
+    and there is no current connection.'''
+    pass
+
+class Connector:
+    '''This class abstracts out a reconnectable client that can connect
+    to controllers and models found in the Juju data files.
+    '''
+    def __init__(self, loop=None, max_frame_size=None, bakery_client=None):
+        '''Initialize a connector that will use the given parameters
+        by default when making a new connection'''
+        self.max_frame_size = max_frame_size
+        self.loop = loop or asyncio.get_event_loop()
+        self.bakery_client = bakery_client
+        self._connection = None
+        self.controller_name = None
+        self.model_name = None
+        self.jujudata = JujuData()
+
+    def is_connected(self):
+        '''Report whether there is a currently connected controller or not'''
+        return self._connection is not None
+
+    def connection(self):
+        '''Return the current connection; raises an exception if there
+        is no current connection.'''
+        if not self.is_connected():
+            raise NoConnectionException('not connected')
+        return self._connection
+
+    async def connect(self, *args, **kw):
+        """Connect to an arbitrary Juju model.
+
+        args and kw are passed through to Connection.connect()
+        """
+        kw.setdefault('loop', self.loop)
+        kw.setdefault('max_frame_size', self.max_frame_size)
+        kw.setdefault('bakery_client', self.bakery_client)
+        self._connection = await Connection.connect(*args, **kw)
+
+    async def disconnect(self):
+        """Shut down the watcher task and close websockets.
+        """
+        if self._connection:
+            log.debug('Closing model connection')
+            await self._connection.close()
+            self._connection = None
+
+    async def connect_controller(self, controller_name=None):
+        """Connect to a controller by name. If the name is empty, it
+        connect to the current controller.
+        """
+        if not controller_name:
+            controller_name = self.jujudata.current_controller()
+        if not controller_name:
+            raise JujuConnectionError('No current controller')
+
+        controller = self.jujudata.controllers()[controller_name]
+        # TODO change Connection so we can pass all the endpoints
+        # instead of just the first.
+        endpoint = controller['api-endpoints'][0]
+        accounts = self.jujudata.accounts()[controller_name]
+
+        await self.connect(
+            endpoint=endpoint,
+            uuid=None,
+            username=accounts.get('user'),
+            password=accounts.get('password'),
+            cacert=controller.get('ca-cert'),
+            bakery_client=self.bakery_client_for_controller(controller_name),
+        )
+        self.controller_name = controller_name
+
+    async def connect_model(self, model_name=None):
+        """Connect to a model by name. If either controller or model
+        parts of the name are empty, the current controller and/or model
+        will be used.
+
+        :param str model: <controller>:<model>
+        """
+
+        try:
+            controller_name, model_name = self.jujudata.parse_model(model_name)
+            controller = self.jujudata.controllers().get(controller_name)
+        except JujuError as e:
+            raise JujuConnectionError(e.message) from e
+        if controller is None:
+            raise JujuConnectionError('Controller {} not found'.format(controller_name))
+        # TODO change Connection so we can pass all the endpoints
+        # instead of just the first one.
+        endpoint = controller['api-endpoints'][0]
+        models = self.jujudata.models()[controller_name]
+        account = self.jujudata.accounts()[controller_name]
+
+        # TODO if there's no record for the required model name, connect
+        # to the controller to find out the model's uuid, then connect
+        # to that. This will let connect_model work with models that
+        # haven't necessarily synced with the local juju data,
+        # and also remove the need for base.CleanModel to
+        # patch JujuData.models with a mock.
+        await self.connect(
+            endpoint=endpoint,
+            uuid=models['models'][model_name]['uuid'],
+            username=account.get('user'),
+            password=account.get('password'),
+            cacert=controller.get('ca-cert'),
+            bakery_client=self.bakery_client_for_controller(controller_name),
+        )
+        self.controller_name = controller_name
+        self.model_name = controller_name + ':' + model_name
+
+    def bakery_client_for_controller(self, controller_name):
+        '''Make a copy of the bakery client with a the appropriate controller's
+        cookiejar in it.
+        '''
+        bakery_client = self.bakery_client
+        if bakery_client:
+            bakery_client = bakery_client.clone()
+        else:
+            bakery_client = httpbakery.Client()
+        bakery_client.cookies = self.jujudata.cookies_for_controller(controller_name)

juju/client/jujudata.py
@@ -1,9 +1,11 @@
 import io
 import os
+import pathlib
 
 import juju.client.client as jujuclient
 import yaml
 from juju import tag
+from juju.client.gocookies import GoCookieJar
 from juju.errors import JujuError
 
 
@@ -66,10 +68,10 @@ class JujuData:
         if '/' not in model_name:
             # model name doesn't include a user prefix, so add one
             # by using the current user for the controller.
-            accounts = self.accounts().get('controller_name')
+            accounts = self.accounts().get(controller_name)
             if accounts is None:
-                raise JujuError('No account found for controller {}'.format(controller_name))
-            username = accounts.get['user']
+                raise JujuError('No account found for controller {} '.format(controller_name))
+            username = accounts.get('user')
             if username is None:
                 raise JujuError('No username found for controller {}'.format(controller_name))
             model_name = username + "/" + model_name
@@ -127,3 +129,11 @@ class JujuData:
             data = yaml.safe_load(f)
             self._loaded[filename] = data
             return data.get(key)
+
+    def cookies_for_controller(self, controller_name):
+        f = pathlib.Path(self.path) / 'cookies' / controller_name
+        if not f.exists():
+            f = pathlib.Path('~/.go-cookies').expanduser()
+            # TODO if neither cookie file exists, where should
+            # we create the cookies?
+        return GoCookieJar(str(f))

tests/integration/test_controller.py
@@ -1,7 +1,7 @@
 import asyncio
 import uuid
 
-from juju.controller import Controller
+from juju.client.connection import Connection
 from juju.errors import JujuAPIError
 
 import pytest
@@ -59,14 +59,18 @@ async def test_change_user_password(event_loop):
         username = 'test-password{}'.format(uuid.uuid4())
         user = await controller.add_user(username)
         await user.set_password('password')
+        # Check that we can connect with the new password.
+        new_connection = None
         try:
-            new_controller = Controller()
-            await new_controller.connect(
-                controller.connection.endpoint, username, 'password')
+            endpoint, kwargs = controller.connection().connect_params()
+            kwargs['username'] = username
+            kwargs['password'] = 'password'
+            new_connection = await Connection.connect(endpoint, **kwargs)
         except JujuAPIError:
             raise AssertionError('Unable to connect with new password')
         finally:
-            await new_controller.disconnect()
+            if new_connection:
+                await new_connection.close()
 
 
 @base.bootstrapped

commit 86fa4fd9904232b76186081139af407f7169cdcd

Merge in fixes and improvements from #204
Squashed commit of the following:

commit 157b176c6bad2d568bffd5eb378e085fd56c07f3
Author: Cory Johns <johnsca@gmail.com>
Date:   Fri Feb 16 11:08:52 2018 -0500

    Update doc string on connect_current_model example

commit 6a50a8d7ce801fcb71d598254e81919f08b7c33e
Author: Cory Johns <johnsca@gmail.com>
Date:   Fri Feb 16 09:43:19 2018 -0500

    Another attempt to fix #172

commit 6e1a9cde4ab31c0bfa873964d58c1a16b8e48f1f
Author: Cory Johns <johnsca@gmail.com>
Date:   Thu Feb 15 16:42:35 2018 -0500

    Maybe fix #172

    The error in #172 is caused by `get_child_watcher()` needing to be
    called in the main thread.  Setting `forbid_global_loop` causes
    pytest-asyncio to atach the loop (see [this
    commit](https://github.com/pytest-dev/pytest-asyncio/commit/150e91353057d158f444d1198781ac346fcfba9d)).
    However, I can't actually find where the threads are being created, so
    I'm not 100% certain that this will be called in the main thread, but
    it seems pretty likely.

commit 240f697afda0f44d5b2178b6cc5e7085f3a51e71
Author: Cory Johns <johnsca@gmail.com>
Date:   Thu Feb 15 16:16:43 2018 -0500

    Only try redirect endpoints for which we have valid macaroons

commit d9300c5ab2a414468df903967100942b2f7f7400
Author: Cory Johns <johnsca@gmail.com>
Date:   Wed Feb 14 14:25:44 2018 -0500

    Try connections in parallel and use first to connect

    The controller may return endpoints that won't succeed (as they may be
    cloud internal addresses) along with one(s) that will.  These can take
    some time to fail, so we should be trying all endpoints at the same time
    and using the first to successfully connect.

    This also prevents the always failing endpoints from masking an error
    connecting to the one that should succeed.

commit 37430da7c0c8af9bd051933db7ea27385c951195
Author: Cory Johns <johnsca@gmail.com>
Date:   Wed Feb 14 14:07:50 2018 -0500

    Fix test for expires on GoCookieJar

commit 3bc649f04a9c57c1af96ab6f8dc77c811ec60240
Author: Cory Johns <johnsca@gmail.com>
Date:   Wed Feb 14 12:48:21 2018 -0500

    Handle cookie files containing `null`

    For the LXD provider, the cookie file contains `null` rather than an
    empty list.

commit b41949d3901d6ee451010ee1c715cded70d6e492
Author: Cory Johns <johnsca@gmail.com>
Date:   Fri Feb 9 13:44:44 2018 -0500

    Fix of macaroon auth broken by #187

examples/connect_current_model.py
@@ -1,0 +1,26 @@
+"""
+This is a very basic example that connects to the currently selected model
+and prints the number of applications deployed to it.
+"""
+import logging
+
+from juju import loop
+from juju.model import Model
+
+log = logging.getLogger(__name__)
+
+
+async def main():
+    model = Model()
+    try:
+        await model.connect_current()
+        print('There are {} applications'.format(len(model.applications)))
+    finally:
+        if model.is_connected():
+            print('Disconnecting from model')
+            await model.disconnect()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    loop.run(main())

juju/client/connection.py
@@ -11,20 +11,11 @@ from http.client import HTTPSConnection
 import macaroonbakery.httpbakery as httpbakery
 import macaroonbakery.bakery as bakery
 import websockets
-from juju import utils
+from juju import errors, tag, utils
 from juju.client import client
-from juju.errors import JujuAPIError, JujuError
 from juju.utils import IdQueue
 
 log = logging.getLogger('juju.client.connection')
-
-
-class _RedirectError(Exception):
-    """_RedirectError is the exception raised from Monitor.login when
-    the controller is redirecting us to another controller
-    :param endpoints list((str, str)): list of (host:port, cacert) pairs"""
-    def __init__(self, endpoints):
-        self.endpoints = endpoints
 
 
 class Monitor:
@@ -79,7 +70,8 @@ class Monitor:
             return self.DISCONNECTING
 
         # connection closed uncleanly (we didn't call connection.close)
-        if connection._receiver_task.stopped.is_set() or not connection.ws.open:
+        stopped = connection._receiver_task.stopped.is_set()
+        if stopped or not connection.ws.open:
             return self.ERROR
 
         # everything is fine!
@@ -139,18 +131,14 @@ class Connection:
         if bakery_client is None:
             bakery_client = httpbakery.Client()
         self.bakery_client = bakery_client
-        if username is not None and '@' in username and not username.endswith('@local'):
-            # We're trying to log in as an external user - we need to use macaroon
-            # authentication with no username or password.
+        if username and '@' in username and not username.endswith('@local'):
+            # We're trying to log in as an external user - we need to use
+            # macaroon authentication with no username or password.
             if password is not None:
-                raise Exception('cannot log in as external user with a password')
+                raise errors.JujuAuthError('cannot log in as external '
+                                           'user with a password')
             username = None
-        self.usertag = username
-        if username is not None and not username.startswith('user-'):
-            # TODO this doesn't seem quite right, as 'user-foo' is
-            # actually a valid username (its tag would be user-user-foo),
-            # but don't break the existing API.
-            self.usertag = 'user-' + username
+        self.usertag = tag.user(username)
         self.password = password
         self.loop = loop or asyncio.get_event_loop()
 
@@ -203,7 +191,7 @@ class Connection:
             ssl=self._get_ssl(cacert),
             loop=self.loop,
             max_size=self.max_frame_size,
-        ), url)
+        ), url, endpoint, cacert)
 
     async def close(self):
         if not self.ws:
@@ -323,7 +311,7 @@ class Connection:
 
         if 'error' in result:
             # API Error Response
-            raise JujuAPIError(result)
+            raise errors.JujuAPIError(result)
 
         if 'response' not in result:
             # This may never happen
@@ -335,15 +323,15 @@ class Connection:
             # Perhaps JujuError should return all the results including
             # errors, or perhaps a keyword parameter to the rpc method
             # could be added to trigger this behaviour.
-            errors = []
+            err_results = []
             for res in result['response']['results']:
                 if res.get('error', {}).get('message'):
-                    errors.append(res['error']['message'])
-            if errors:
-                raise JujuError(errors)
+                    err_results.append(res['error']['message'])
+            if err_results:
+                raise errors.JujuError(err_results)
 
         elif result['response'].get('error', {}).get('message'):
-            raise JujuError(result['response']['error']['message'])
+            raise errors.JujuError(result['response']['error']['message'])
 
         return result
 
@@ -401,10 +389,11 @@ class Connection:
         return await Connection.connect(**self.connect_params())
 
     def connect_params(self):
-        """Return a tuple of parameters suitable for passing to Connection.connect that
-        can be used to make a new connection to the same controller (and model
-        if specified. The first element in the returned tuple holds the endpoint argument; the other
-        holds a dict of the keyword args.
+        """Return a tuple of parameters suitable for passing to
+        Connection.connect that can be used to make a new connection
+        to the same controller (and model if specified. The first
+        element in the returned tuple holds the endpoint argument;
+        the other holds a dict of the keyword args.
         """
         return {
             'endpoint': self.endpoint,
@@ -442,22 +431,38 @@ class Connection:
 
     async def _connect(self, endpoints):
         if len(endpoints) == 0:
-            raise Exception('no endpoints to connect to')
-        first_exception = None
-        # TODO try connecting concurrently.
-        for endpoint, cacert in endpoints:
+            raise errors.JujuConnectionError('no endpoints to connect to')
+
+        async def _try_endpoint(endpoint, cacert, delay):
+            if delay:
+                await asyncio.sleep(delay)
+            return await self._open(endpoint, cacert)
+
+        # Try all endpoints in parallel, with slight increasing delay (+100ms
+        # for each subsequent endpoint); the delay allows us to prefer the
+        # earlier endpoints over the latter. Use first successful connection.
+        tasks = [self.loop.create_task(_try_endpoint(endpoint, cacert,
+                                                     0.1 * i))
+                 for i, (endpoint, cacert) in enumerate(endpoints)]
+        for task in asyncio.as_completed(tasks, loop=self.loop):
             try:
-                self.ws, self.addr = await self._open(endpoint, cacert)
-                self.endpoint = endpoint
-                self.cacert = cacert
-                self._receiver_task.start()
-                log.info("Driver connected to juju %s", self.addr)
-                self.monitor.close_called.clear()
-                return
-            except Exception as e:
-                if first_exception is None:
-                    first_exception = e
-        raise first_exception
+                result = await task
+                break
+            except ConnectionError:
+                continue  # ignore; try another endpoint
+        else:
+            raise errors.JujuConnectionError(
+                'Unable to connect to any endpoint: {}'.format(', '.join([
+                    endpoint for endpoint, cacert in endpoints])))
+        for task in tasks:
+            task.cancel()
+        self.ws = result[0]
+        self.addr = result[1]
+        self.endpoint = result[2]
+        self.cacert = result[3]
+        self._receiver_task.start()
+        log.info("Driver connected to juju %s", self.addr)
+        self.monitor.close_called.clear()
 
     async def _connect_with_login(self, endpoints):
         """Connect to the websocket.
@@ -493,7 +498,8 @@ class Connection:
                     # note: remove the port number.
                     'https://' + self.endpoint + '/',
                 )
-            raise Exception('failed to authenticate after several attempts')
+            raise errors.JujuAuthError('failed to authenticate '
+                                       'after several attempts')
         finally:
             if not success:
                 await self.close()
@@ -501,7 +507,7 @@ class Connection:
     async def _connect_with_redirect(self, endpoints):
         try:
             login_result = await self._connect_with_login(endpoints)
-        except _RedirectError as e:
+        except errors.JujuRedirectException as e:
             login_result = await self._connect_with_login(e.endpoints)
         self._build_facades(login_result.get('facades', {}))
         self._pinger_task.start()
@@ -513,12 +519,14 @@ class Connection:
 
     async def login(self):
         params = {}
-        macaroons = _macaroons_for_domain(self.bakery_client.cookies, self.endpoint)
-        if self.usertag:
+        if self.password:
             params['auth-tag'] = self.usertag
             params['credentials'] = self.password
         else:
-            params['macaroons'] = [[bakery.macaroon_to_dict(m) for m in ms] for ms in macaroons]
+            macaroons = _macaroons_for_domain(self.bakery_client.cookies,
+                                              self.endpoint)
+            params['macaroons'] = [[bakery.macaroon_to_dict(m) for m in ms]
+                                   for ms in macaroons]
 
         try:
             return await self.rpc({
@@ -527,7 +535,7 @@ class Connection:
                 "version": 3,
                 "params": params,
             })
-        except JujuAPIError as e:
+        except errors.JujuAPIError as e:
             if e.error_code != 'redirection required':
                 raise
             log.info('Controller requested redirect')
@@ -539,14 +547,7 @@ class Connection:
                 "request": "RedirectInfo",
                 "version": 3,
             }))['response']
-            cacert = redirect_info['ca-cert']
-            raise _RedirectError(
-                endpoints=[
-                    ('{value}:{port}'.format(**s), cacert)
-                    for servers in redirect_info['servers']
-                    for s in servers if s['scope'] == 'public'
-                ],
-            )
+            raise errors.JujuRedirectException(redirect_info) from e
 
 
 class _Task:

juju/client/connector.py
@@ -5,7 +5,7 @@ import copy
 import macaroonbakery.httpbakery as httpbakery
 from juju.client.connection import Connection
 from juju.client.jujudata import FileJujuData
-from juju.errors import JujuConnectionError, JujuError
+from juju.errors import JujuAuthError, JujuConnectionError, JujuError
 
 log = logging.getLogger('connector')
 
@@ -105,12 +105,19 @@ class Connector:
         except JujuError as e:
             raise JujuConnectionError(e.message) from e
         if controller is None:
-            raise JujuConnectionError('Controller {} not found'.format(controller_name))
+            raise JujuConnectionError('Controller {} not found'.format(
+                controller_name))
         # TODO change Connection so we can pass all the endpoints
         # instead of just the first one.
         endpoint = controller['api-endpoints'][0]
-        models = self.jujudata.models()[controller_name]
-        account = self.jujudata.accounts().get(controller_name, {})
+        accounts = self.jujudata.accounts()
+        if controller_name not in accounts:
+            raise JujuAuthError('Account not found for controller: {}'.format(
+                controller_name))
+        account = accounts[controller_name]
+        models = self.jujudata.models().get(controller_name, {}).get('models', {})
+        if model_name not in models:
+            raise JujuConnectionError('Model not found: {}'.format(model_name))
 
         # TODO if there's no record for the required model name, connect
         # to the controller to find out the model's uuid, then connect
@@ -120,7 +127,7 @@ class Connector:
         # subclass JujuData.
         await self.connect(
             endpoint=endpoint,
-            uuid=models['models'][model_name]['uuid'],
+            uuid=models[model_name]['uuid'],
             username=account.get('user'),
             password=account.get('password'),
             cacert=controller.get('ca-cert'),

juju/client/gocookies.py
@@ -13,8 +13,9 @@ class GoCookieJar(cookiejar.FileCookieJar):
     def _really_load(self, f, filename, ignore_discard, ignore_expires):
         '''Implement the _really_load method called by FileCookieJar
         to implement the actual cookie loading'''
+        data = json.load(f) or []
         now = time.time()
-        for cookie in map(_new_py_cookie, json.load(f)):
+        for cookie in map(_new_py_cookie, data):
             if not ignore_expires and cookie.is_expired(now):
                 continue
             self.set_cookie(cookie)
@@ -46,7 +47,7 @@ def _new_py_cookie(go_cookie):
     expires = None
     if go_cookie.get('Expires') is not None:
         t = pyrfc3339.parse(go_cookie['Expires'])
-        expires = t.strftime("%s")
+        expires = t.timestamp()
     return cookiejar.Cookie(
         version=0,
         name=go_cookie['Name'],

juju/client/jujudata.py
@@ -209,9 +209,11 @@ class FileJujuData(JujuData):
             return data.get(key)
 
     def cookies_for_controller(self, controller_name):
-        f = pathlib.Path(self.path) / 'cookies' / controller_name
+        f = pathlib.Path(self.path) / 'cookies' / (controller_name + '.json')
         if not f.exists():
             f = pathlib.Path('~/.go-cookies').expanduser()
             # TODO if neither cookie file exists, where should
             # we create the cookies?
-        return GoCookieJar(str(f))
+        jar = GoCookieJar(str(f))
+        jar.load()
+        return jar

juju/errors.py
@@ -25,3 +25,25 @@ class JujuAPIError(JujuError):
 
 class JujuConnectionError(ConnectionError, JujuError):
     pass
+
+
+class JujuAuthError(JujuConnectionError):
+    pass
+
+
+class JujuRedirectException(Exception):
+    """Exception indicating that a redirection was requested"""
+    def __init__(self, redirect_info):
+        self.redirect_info = redirect_info
+
+    @property
+    def ca_cert(self):
+        return self.redirect_info['ca-cert']
+
+    @property
+    def endpoints(self):
+        return [
+            ('{value}:{port}'.format(**s), self.ca_cert)
+            for servers in self.redirect_info['servers']
+            for s in servers if s['scope'] == 'public'
+        ]

juju/tag.py
@@ -1,3 +1,8 @@
+# TODO: Tags should be a proper class, so that we can distinguish whether
+# something is already a tag or not.  For example, 'user-foo' is a valid
+# username, but is ambiguous with the already-tagged username 'foo'.
+
+
 def _prefix(prefix, s):
     if s and not s.startswith(prefix):
         return '{}{}'.format(prefix, s)

tests/integration/test_controller.py
@@ -126,6 +126,11 @@ async def test_get_model(event_loop):
             await controller.destroy_model(model_name)
 
 
+async def _wait_for_model(controller, model_name):
+    while model_name not in await controller.list_models():
+        await asyncio.sleep(0.5, loop=controller.loop)
+
+
 async def _wait_for_model_gone(controller, model_name):
     while model_name in await controller.list_models():
         await asyncio.sleep(0.5, loop=controller.loop)
@@ -138,6 +143,9 @@ async def test_destroy_model_by_name(event_loop):
         model_name = 'test-{}'.format(uuid.uuid4())
         model = await controller.add_model(model_name)
         await model.disconnect()
+        await asyncio.wait_for(_wait_for_model(controller,
+                                               model_name),
+                               timeout=60)
         await controller.destroy_model(model_name)
         await asyncio.wait_for(_wait_for_model_gone(controller,
                                                     model_name),
@@ -152,6 +160,9 @@ async def test_add_destroy_model_by_uuid(event_loop):
         model = await controller.add_model(model_name)
         model_uuid = model.info.uuid
         await model.disconnect()
+        await asyncio.wait_for(_wait_for_model(controller,
+                                               model_name),
+                               timeout=60)
         await controller.destroy_model(model_uuid)
         await asyncio.wait_for(_wait_for_model_gone(controller,
                                                     model_name),

tests/integration/test_machine.py
@@ -42,6 +42,11 @@ async def test_status(event_loop):
 @base.bootstrapped
 @pytest.mark.asyncio
 async def test_scp(event_loop):
+    # ensure that asyncio.subprocess will work;
+    try:
+        asyncio.get_child_watcher().attach_loop(event_loop)
+    except RuntimeError:
+        pytest.skip('test_scp will always fail outside of MainThread')
     async with base.CleanModel() as model:
         await model.add_machine()
         await asyncio.wait_for(

tests/integration/test_unit.py
@@ -52,6 +52,11 @@ async def test_run_action(event_loop):
 @base.bootstrapped
 @pytest.mark.asyncio
 async def test_scp(event_loop):
+    # ensure that asyncio.subprocess will work;
+    try:
+        asyncio.get_child_watcher().attach_loop(event_loop)
+    except RuntimeError:
+        pytest.skip('test_scp will always fail outside of MainThread')
     async with base.CleanModel() as model:
         app = await model.deploy('ubuntu')
 