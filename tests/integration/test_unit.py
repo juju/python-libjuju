d099553d1ce4b07e64a0017536d609ef8ea685a0
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-07-02 13:01:43 +0100

    Change test to use default directory

--- tests/integration/test_unit.py
+++ tests/integration/test_unit.py
@@ -123,8 +123,8 @@ async def test_ssh(event_loop):
                                        machine.agent_status == 'started')),
             timeout=480)
 
-        await unit.ssh("\"echo 'test' > ~/test.txt\"")
+        await unit.ssh("\"echo 'test' > test.txt\"")
 
         with NamedTemporaryFile() as f:
-            await unit.scp_from('~/test.txt', f.name)
+            await unit.scp_from('test.txt', f.name)
             assert f.read() == b'test'
