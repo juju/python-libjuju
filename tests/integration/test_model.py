52ae745c5d09b7de054e2dc6da3a94cd74d6bd45
Caner Derici <caner.derici@canonical.com>
2021-09-02 11:03:52 -0600

    improved feedback on failure

--- tests/integration/test_model.py
+++ tests/integration/test_model.py
@@ -327,30 +327,29 @@ async def test_add_manual_machine_ssh(event_loop):
         # and pylxd's container.execute seems to be broken and fails and/or
         # hangs trying to properly check if the service is up.
         time.sleep(5)
-
+        spec = 'ssh:{}@{}:{}'.format(
+            test_user,
+            host['address'],
+            private_key_path,
+        )
+        err = None
         for attempt in range(1, 4):
             try:
                 # add a new manual machine
-                machine1 = await model.add_machine(spec='ssh:{}@{}:{}'.format(
-                    test_user,
-                    host['address'],
-                    private_key_path,
-                ))
-            except paramiko.ssh_exception.NoValidConnectionsError:
+                machine1 = await model.add_machine(spec=spec)
+            except (paramiko.ssh_exception.NoValidConnectionsError, paramiko.ssh_exception.AuthenticationException) as e:
                 # retry the ssh connection a few times if it fails
+                err = e
                 time.sleep(attempt * 5)
             else:
+                # try part finished without exception, breaking
                 break
-        else:
-            # this else will only run when for finishes without a
-            # break (meaning it kept erroring)
-            raise Exception('Unable to add_machine')
 
-        assert len(model.machines) == 1
+        assert len(model.machines) == 1, 'Unable to add_machine in %s attempts with spec : %s -- exception was %s' % (attempt, spec, err)
 
         res = await machine1.destroy(force=True)
 
-        assert res is None
+        assert res is None, 'Bad teardown, res is : %s' % res
         assert len(model.machines) == 0
 
         container.stop(wait=True)
@@ -445,31 +444,29 @@ async def test_add_manual_machine_ssh_root(event_loop):
         # and pylxd's container.execute seems to be broken and fails and/or
         # hangs trying to properly check if the service is up.
         time.sleep(5)
-
+        spec = 'ssh:{}@{}:{}'.format(
+            "root",
+            host['address'],
+            private_key_path,
+        )
+        err = None
         for attempt in range(1, 4):
             try:
                 # add a new manual machine
-                machine1 = await model.add_machine(spec='ssh:{}@{}:{}'.format(
-                    "root",
-                    host['address'],
-                    private_key_path,
-                ))
-            except (paramiko.ssh_exception.NoValidConnectionsError, paramiko.ssh_exception.AuthenticationException):
+                machine1 = await model.add_machine(spec=spec)
+            except (paramiko.ssh_exception.NoValidConnectionsError, paramiko.ssh_exception.AuthenticationException) as e:
                 # if we get an exception, try again
+                err = e
                 time.sleep(attempt * 5)
             else:
                 # try part finished without exception
                 break
-        else:
-            # this else will only run when for finishes without a
-            # break (meaning it kept erroring)
-            raise Exception('Unable to add_machine')
 
-        assert len(model.machines) == 1
+        assert len(model.machines) == 1, 'Unable to add_machine in %s attempts with spec : %s -- exception was %s' % (attempt, spec, err)
 
         res = await machine1.destroy(force=True)
 
-        assert res is None
+        assert res is None, 'Bad teardown, res is : %s' % res
         assert len(model.machines) == 0
 
         container.stop(wait=True)
