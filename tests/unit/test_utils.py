a71bb34e0b532a9ca175ded091cbdcf8c61dd181
Caner Derici <caner.derici@canonical.com>
2021-10-05 12:04:57 -0600

    fix unit tests
    ... to be actual unit tests (as opposed to being integration tests
    that tests the actual setup in which it's being run)

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,27 +1,14 @@
-import os
+import unittest
 
-from pathlib import Path
 from juju.utils import juju_config_dir, juju_ssh_key_paths
 
 
-def test_config_dir():
-    config_dir = juju_config_dir()
-    assert os.path.exists(config_dir)
-    assert os.path.isdir(config_dir)
+class TestDirResolve(unittest.TestCase):
+    def test_config_dir(self):
+        config_dir = juju_config_dir()
+        assert 'local/share/juju' in config_dir
 
-    assert os.path.exists(os.path.join(config_dir, 'controllers.yaml'))
-
-
-def test_juju_ssh_key_paths():
-    public, private = juju_ssh_key_paths()
-    assert os.path.exists(public)
-    assert os.path.isfile(public)
-
-    assert os.path.exists(private)
-    assert os.path.isfile(private)
-
-    with Path(public).open('r') as f:
-        assert "ssh-rsa" in f.read()
-
-    with Path(private).open('r') as g:
-        assert "PRIVATE KEY" in g.read()
+    def test_juju_ssh_key_paths(self):
+        public, private = juju_ssh_key_paths()
+        assert public.endswith('ssh/juju_id_rsa.pub')
+        assert private.endswith('ssh/juju_id_rsa')
