55f6900390fc87391ca17c39ea5aab8b354b1775
Caner Derici <caner.derici@canonical.com>
2021-10-18 16:30:59 -0600

    revise test_crossmodel

--- tests/integration/test_crossmodel.py
+++ tests/integration/test_crossmodel.py
@@ -4,22 +4,21 @@ from pathlib import Path
 import pytest
 
 from .. import base
+from juju import jasyncio
 
 
 @base.bootstrapped
 @pytest.mark.asyncio
 async def test_offer(event_loop):
     async with base.CleanModel() as model:
-        application = await model.deploy(
+        await model.deploy(
             'cs:~jameinel/ubuntu-lite-7',
             application_name='ubuntu',
             series='bionic',
             channel='stable',
         )
         assert 'ubuntu' in model.applications
-        await model.block_until(
-            lambda: all(unit.workload_status == 'active'
-                        for unit in application.units))
+        await model.wait_for_idle(status="active")
         await model.create_offer("ubuntu:ubuntu")
 
         offers = await model.list_offers()
@@ -33,16 +32,14 @@ async def test_offer(event_loop):
 @pytest.mark.asyncio
 async def test_consume(event_loop):
     async with base.CleanModel() as model_1:
-        application = await model_1.deploy(
+        await model_1.deploy(
             'cs:~jameinel/ubuntu-lite-7',
             application_name='ubuntu',
             series='bionic',
             channel='stable',
         )
         assert 'ubuntu' in model_1.applications
-        await model_1.block_until(
-            lambda: all(unit.workload_status == 'active'
-                        for unit in application.units))
+        await model_1.wait_for_idle(status="active")
         await model_1.create_offer("ubuntu:ubuntu")
 
         offers = await model_1.list_offers()
@@ -65,16 +62,14 @@ async def test_consume(event_loop):
 @pytest.mark.asyncio
 async def test_remove_saas(event_loop):
     async with base.CleanModel() as model_1:
-        application = await model_1.deploy(
+        await model_1.deploy(
             'cs:~jameinel/ubuntu-lite-7',
             application_name='ubuntu',
             series='bionic',
             channel='stable',
         )
         assert 'ubuntu' in model_1.applications
-        await model_1.block_until(
-            lambda: all(unit.workload_status == 'active'
-                        for unit in application.units))
+        await model_1.wait_for_idle(status="active")
         await model_1.create_offer("ubuntu:ubuntu")
 
         offers = await model_1.list_offers()
@@ -86,11 +81,8 @@ async def test_remove_saas(event_loop):
         async with base.CleanModel() as model_2:
             await model_2.consume("admin/{}.ubuntu".format(model_1.info.name))
 
-            status = await model_2.get_status()
-            if 'ubuntu' not in status.remote_applications:
-                raise Exception("Expected ubuntu in saas")
-
             await model_2.remove_saas('ubuntu')
+            await jasyncio.sleep(5)
 
             status = await model_2.get_status()
             if 'ubuntu' in status.remote_applications:
@@ -110,9 +102,7 @@ async def test_add_relation_with_offer(event_loop):
             channel='stable',
         )
         assert 'mysql' in model_1.applications
-        await model_1.block_until(
-            lambda: all(unit.workload_status == 'active'
-                        for unit in application.units))
+        await model_1.wait_for_idle(status="active")
         await model_1.create_offer("mysql:db")
 
         offers = await model_1.list_offers()
@@ -139,6 +129,7 @@ async def test_add_relation_with_offer(event_loop):
                 raise Exception("Expected mysql in saas")
 
             await model_2.remove_saas('mysql')
+            await jasyncio.sleep(5)
 
             status = await model_2.get_status()
             if 'mysql' in status.remote_applications:
