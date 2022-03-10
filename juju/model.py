a69f8fba89939c984db53ced0ad598a69ae4aa6e
Caner Derici <caner.derici@canonical.com>
2022-03-10 10:06:30 -0700

    Ensure num_units is adjusted correctly for subordinate charms
    Subordinate charms are by definition don't require additional
    units. So num-units should be 0 at deploy.
    
    Normally in libjuju num_units defaults to 1, this change lets libjuju
    to check the metadata from charmhub to see if we're deploying a
    subordinate charm. If that's the case then we adjust the num-units to
    0 to be able to deploy it without any problems from Juju's side.
    
    fixes #639

--- juju/model.py
+++ juju/model.py
@@ -1622,6 +1622,16 @@ class Model:
                     resources = await self._add_charmhub_resources(res.app_name,
                                                                    identifier,
                                                                    add_charm_res.charm_origin)
+                    charm_info = await self.charmhub.info(res.app_name)
+                    is_subordinate = False
+                    try:
+                        is_subordinate = charm_info.result.charm.subordinate
+                    except AttributeError:
+                        log.warning('CharmHub.Info : unable to retrieve the subordinate information')
+                    if is_subordinate:
+                        if num_units > 1:
+                            raise JujuError("cannot use num_units with subordinate application")
+                        num_units = 0
 
                 if Schema.CHARM_STORE.matches(url.schema):
                     resources = await self._add_store_resources(res.app_name,
