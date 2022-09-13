fbdab0996fc0289aafb5bf59e0605efc20cc498a
Caner Derici <caner.derici@canonical.com>
2022-09-13 11:10:44 -0600

    Add list_resources method for charmhub module

--- juju/charmhub.py
+++ juju/charmhub.py
@@ -6,6 +6,47 @@ class CharmHub:
     def __init__(self, model):
         self.model = model
 
+    def request_charmhub_with_retry(self, url, retries):
+        for attempt in range(retries):
+            _response = requests.get(url)
+            if _response.status_code == 200:
+                return _response
+            jasyncio.sleep(5)
+        raise JujuError("Got {} from {}".format(_response.status_code, url))
+
+    async def get_charm_id(self, charm_name):
+        conn, headers, path_prefix = self.model.connection().https_connection()
+
+        model_conf = await self.model.get_config()
+        charmhub_url = model_conf['charmhub-url']
+        url = "{}/v2/charms/info/{}".format(charmhub_url.value, charm_name)
+        _response = self.request_charmhub_with_retry(url, 5)
+        response = json.loads(_response.text)
+        return response['id'], response['name']
+
+    async def is_subordinate(self, charm_name):
+        conn, headers, path_prefix = self.model.connection().https_connection()
+
+        model_conf = await self.model.get_config()
+        charmhub_url = model_conf['charmhub-url']
+        url = "{}/v2/charms/info/{}?fields=default-release.revision.subordinate".format(charmhub_url.value, charm_name)
+        _response = self.request_charmhub_with_retry(url, 5)
+        response = json.loads(_response.text)
+        return 'subordinate' in response['default-release']['revision']
+
+    # TODO (caner) : we should be able to recreate the channel-map through the
+    #  api call without needing the CharmHub facade
+
+    async def list_resources(self, charm_name):
+        conn, headers, path_prefix = self.model.connection().https_connection()
+
+        model_conf = await self.model.get_config()
+        charmhub_url = model_conf['charmhub-url']
+        url = "{}/v2/charms/info/{}?fields=default-release.resources".format(charmhub_url.value, charm_name)
+        _response = self.request_charmhub_with_retry(url, 5)
+        response = json.loads(_response.text)
+        return response['default-release']['resources']
+
     async def info(self, name, channel=None):
         """info displays detailed information about a CharmHub charm. The charm
         can be specified by the exact name.
