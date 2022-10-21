bfcbc0ba968555e317909972c28101b149854b0c
Caner Derici <caner.derici@canonical.com>
2022-10-21 12:24:06 -0600

    Make sure the action status is correctly set
    fixes #719

--- juju/action.py
+++ juju/action.py
@@ -6,13 +6,16 @@ class Action(model.ModelEntity):
     def __init__(self, entity_id, model, history_index=-1, connected=True):
         super().__init__(entity_id, model, history_index, connected)
         self.results = {}
+        self._status = self.data['status']
 
     @property
     def status(self):
-        return self.data['status']
+        return self._status
 
     async def fetch_output(self):
-        self.results = await self.model.get_action_output(self.id)
+        completed_action = await self.model._get_completed_action(self.id)
+        self.results = completed_action.output
+        self._status = completed_action.status
 
     async def wait(self):
         self.results or await self.fetch_output()
