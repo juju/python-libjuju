fcb9de2f097f00fb15d38dc44bff7232802192e7
Caner Derici <caner.derici@canonical.com>
2023-01-31 16:00:04 -0700

    Add utility to parse base argument

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
 
-from juju.utils import juju_config_dir, juju_ssh_key_paths
+from juju.utils import juju_config_dir, juju_ssh_key_paths, parse_base_arg
+from juju.client import client
 
 
 class TestDirResolve(unittest.TestCase):
@@ -12,3 +13,9 @@ class TestDirResolve(unittest.TestCase):
         public, private = juju_ssh_key_paths()
         assert public.endswith('ssh/juju_id_rsa.pub')
         assert private.endswith('ssh/juju_id_rsa')
+
+    def test_parse_base_arg(self):
+        base = parse_base_arg('ubuntu@22.04')
+        assert isinstance(base, client.Base)
+        assert base.name == 'ubuntu'
+        assert base.channel == '22.04'
