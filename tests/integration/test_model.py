e7a918bafcbc1a41ea313831db20748e36dfa06c
Caner Derici <caner.derici@canonical.com>
2023-04-26 17:20:41 -0600

    Fix test_deploy_local_bundle_include_file

--- tests/integration/test_model.py
+++ tests/integration/test_model.py
@@ -88,12 +88,12 @@ async def test_deploy_local_bundle_include_file(event_loop):
     async with base.CleanModel() as model:
         await model.deploy(str(bundle_yaml_path))
 
-        mysql = model.applications.get('mysql', None)
-        ghost = model.applications.get('ghost', None)
+        appa = model.applications.get('helloa', None)
+        appb = model.applications.get('hellob', None)
         test = model.applications.get('test', None)
-        assert mysql and ghost and test
-        assert ghost.config.get('port', None) == 2369
-        assert ghost.config.get('url', "") == 'http://my-ghost.blg'
+        assert appa and appb and test
+        assert appa.config.get('port', None) == 666
+        assert appa.config.get('application-repo', "") == "http://my-juju.com"
 
 
 @base.bootstrapped
