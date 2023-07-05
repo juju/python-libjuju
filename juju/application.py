juju/python-libjuju

commit 5d843ec1358e8bea624077fc3b4fe4932b6715e3

Revise the upgrade local charm to use the GetCharmURLOrigin
instead of passing channel to the SetCharm

juju/application.py
@@ -611,10 +611,6 @@ class Application(model.ModelEntity):
         :param str switch: Crossgrade charm url
 
         """
-        if path is not None:
-            await self.local_refresh(channel, force, force_series, force_units,
-                                     path, resources)
-            return
         if resources is not None:
             raise NotImplementedError("resources option is not implemented")
 
@@ -636,6 +632,11 @@ class Application(model.ModelEntity):
             raise JujuError(f'{err.code} : {err.message}')
         charm_url = switch or charm_url_origin_result.url
         origin = charm_url_origin_result.charm_origin
+
+        if path is not None:
+            await self.local_refresh(origin, force, force_series,
+                                     force_units, path, resources)
+            return
 
         parsed_url = URL.parse(charm_url)
         charm_name = parsed_url.name
@@ -766,7 +767,8 @@ class Application(model.ModelEntity):
     upgrade_charm = refresh
 
     async def local_refresh(
-            self, channel=None, force=False, force_series=False, force_units=False,
+            self, charm_origin=None, force=False, force_series=False,
+            force_units=False,
             path=None, resources=None):
         """Refresh the charm for this application with a local charm.
 
@@ -809,7 +811,7 @@ class Application(model.ModelEntity):
         # Update application
         await app_facade.SetCharm(
             application=self.entity_id,
-            channel=channel,
+            charm_origin=charm_origin,
             charm_url=charm_url,
             config_settings=None,
             config_settings_yaml=None,

commit e2e60ae4452f05e7699e4ba200dc1a220a05cbf9

Pass through local_refresh if refreshing with local charm & resources
This patches a bug that
https://github.com/juju/python-libjuju/commit/5d843ec1358e8bea624077fc3b4fe4932b6715e3
introduced, where if --path is given (refreshing with a local charm),
it errors with NYI if --resources is given (instead of falling through
the local_refresh).

Should fix #881

juju/application.py
@@ -623,7 +623,7 @@ class Application(model.ModelEntity):
 
     async def refresh(
             self, channel=None, force=False, force_series=False, force_units=False,
-            path=None, resources=None, revision=None, switch=None):
+            local_charm_path=None, resources=None, revision=None, switch=None):
         """Refresh the charm for this application.
 
         :param str channel: Channel to use when getting the charm from the
@@ -632,17 +632,17 @@ class Application(model.ModelEntity):
             application is not supported by the new charm
         :param bool force_units: Refresh all units immediately, even if in
             error state
-        :param str path: Refresh to a charm located at path
+        :param str local_charm_path: Refresh to a charm located at path
         :param dict resources: Dictionary of resource name/filepath pairs
         :param int revision: Explicit refresh revision
         :param str switch: Crossgrade charm url
 
         """
-        if resources is not None:
-            raise NotImplementedError("resources option is not implemented")
+        if local_charm_path is None and resources is not None:
+            raise NotImplementedError("refreshing a non-local charm with resources option is not yet implemented")
 
         if switch is not None and revision is not None:
-            raise ValueError("switch and revision are mutually exclusive")
+            raise ValueError("switch and revision parameters are mutually exclusive in application refresh")
 
         app_facade = self._facade()
         resources_facade = client.ResourcesFacade.from_connection(self.connection)
@@ -660,9 +660,9 @@ class Application(model.ModelEntity):
         charm_url = switch or charm_url_origin_result.url
         origin = charm_url_origin_result.charm_origin
 
-        if path is not None:
+        if local_charm_path is not None:
             await self.local_refresh(origin, force, force_series,
-                                     force_units, path, resources)
+                                     force_units, local_charm_path, resources)
             return
 
         parsed_url = URL.parse(charm_url)