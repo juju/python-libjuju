22eef0e7df4a10583f3787e31d73208f8e902121
Caner Derici <caner.derici@canonical.com>
2023-11-28 16:52:00 -0700

    Add unit tests for read_secret_data

--- tests/unit/test_secrets.py
+++ tests/unit/test_secrets.py
@@ -1,12 +1,13 @@
 # Copyright 2023 Canonical Ltd.
 # Licensed under the Apache V2, see LICENCE file for details.
 
+import os
 from pathlib import Path
 import tempfile
 import unittest
 import pytest
 
-from juju.secrets import create_secret_data
+from juju.secrets import create_secret_data, read_secret_data
 
 
 class TestCreateSecretData(unittest.TestCase):
@@ -56,4 +57,51 @@ class TestCreateSecretData(unittest.TestCase):
                 "key2": (
                     'ICAgICAgICAgIC0tLS0tQkVHSU4gQ0VSVElGSUNBVEUtLS0tLQogICAgICAgICAgTUlJRllqQ0NBMHFnQXdJQkFnSVFLYVBORDlZZ2dJRzYrak9jZ21wazNEQU5CZ2txaGtpRzl3MEJBUXNGQURBegogICAgICAgICAgTVJ3d0dnWURWUVFLRXhOc2FXNTFlR052Ym5SaGFXNWxjbk11YjNKbk1STXdFUVlEVlFRRERBcDBhVzFBWld4MwogICAgICAgICAgLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQ=='
                 ),
+            })
+
+
+class TestReadSecretData(unittest.TestCase):
+    def test_yaml_file(self):
+        data = """
+        hello: world
+        goodbye#base64: world
+        another-key: !!binary |
+          R0lGODlhDAAMAIQAAP//9/X17unp5WZmZgAAAOfn515eXvPz7Y6OjuDg4J+fn5
+          OTk6enp56enmlpaWNjY6Ojo4SEhP/++f/++f/++f/++f/++f/++f/++f/++f/+
+          +f/++f/++f/++f/++f/++SH+Dk1hZGUgd2l0aCBHSU1QACwAAAAADAAMAAAFLC
+          AgjoEwnuNAFOhpEMTRiggcz4BNJHrv/zCFcLiwMWYNG84BwwEeECcgggoBADs=
+        """
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = os.path.join(temp_dir, "secret.yaml")
+
+            with open(file_path, 'w') as file:
+                file.write(data)
+
+            attrs = read_secret_data(file_path)
+            self.assertDictEqual(attrs, {
+                "hello": "d29ybGQ=",
+                "goodbye": "world",
+                "another-key": "YiJHSUY4OWFceDBjXHgwMFx4MGNceDAwXHg4NFx4MDBceDAwXHhmZlx4ZmZceGY3XHhmNVx4ZjVceGVlXHhlOVx4ZTlceGU1ZmZmXHgwMFx4MDBceDAwXHhlN1x4ZTdceGU3Xl5eXHhmM1x4ZjNceGVkXHg4ZVx4OGVceDhlXHhlMFx4ZTBceGUwXHg5Zlx4OWZceDlmXHg5M1x4OTNceDkzXHhhN1x4YTdceGE3XHg5ZVx4OWVceDllaWlpY2NjXHhhM1x4YTNceGEzXHg4NFx4ODRceDg0XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5XHhmZlx4ZmVceGY5IVx4ZmVceDBlTWFkZSB3aXRoIEdJTVBceDAwLFx4MDBceDAwXHgwMFx4MDBceDBjXHgwMFx4MGNceDAwXHgwMFx4MDUsICBceDhlXHg4MTBceDllXHhlM0BceDE0XHhlOGlceDEwXHhjNFx4ZDFceDhhXHgwOFx4MWNceGNmXHg4ME0kelx4ZWZceGZmMFx4ODVwXHhiOFx4YjAxZlxyXHgxYlx4Y2VceDAxXHhjM1x4MDFceDFlXHgxMCcgXHg4MlxuXHgwMVx4MDA7Ig=="
+            })
+
+    def test_json_file(self):
+        data = '''
+        {
+            "hello": "world",
+            "goodbye#base64": "world"
+        }
+        '''
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = os.path.join(temp_dir, "secret.json")
+
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(data)
+
+            attrs = read_secret_data(file_path)
+
+            self.assertDictEqual(attrs, {
+                "hello": "d29ybGQ=",
+                "goodbye": "world"
             })
