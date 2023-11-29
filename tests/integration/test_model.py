1a42bcc4a9fe09e96641644621affe4c8c809181
Caner Derici <caner.derici@canonical.com>
2023-11-29 14:19:48 -0700

    Move list_secret integration test into test_secrets

--- tests/integration/test_model.py
+++ tests/integration/test_model.py
@@ -1273,23 +1273,3 @@ async def test_storage_pools_on_lxd(event_loop):
         await jasyncio.sleep(5)
         pools = await model.list_storage_pools()
         assert "test-pool" not in [p['name'] for p in pools]
-
-
-# This test can only work if we can fully upgrade the whole charm
-# with the corresponding logic :)
-@base.bootstrapped
-async def test_list_secrets(event_loop):
-    """Use the charm-secret charm definition and see if the
-    arguments defined in the secret are correct or not."""
-
-    charm_path = TESTS_DIR / 'charm-secret/charm-secret_ubuntu-22.04-amd64.charm'
-
-    async with base.CleanModel() as model:
-        await model.deploy(str(charm_path))
-        assert 'charm-secret' in model.applications
-        await model.wait_for_idle(status="active")
-        assert model.units['charm-secret/0'].workload_status == 'active'
-
-        secrets = await model.list_secrets(show_secrets=True)
-        assert secrets is not None
-        assert len(secrets.results) == 1
--- tests/integration/test_secrets.py
+++ tests/integration/test_secrets.py
@@ -3,6 +3,7 @@
 
 import pytest
 from .. import base
+from ..utils import TESTS_DIR
 
 
 @base.bootstrapped
@@ -16,3 +17,23 @@ async def test_add_secret(event_loop):
         secrets = await model.list_secrets()
         assert len(secrets) == 1
         assert secrets[0].label == 'my-apitoken'
+
+
+# This test can only work if we can fully upgrade the whole charm
+# with the corresponding logic :)
+@base.bootstrapped
+async def test_list_secrets(event_loop):
+    """Use the charm-secret charm definition and see if the
+    arguments defined in the secret are correct or not."""
+
+    charm_path = TESTS_DIR / 'charm-secret/charm-secret_ubuntu-22.04-amd64.charm'
+
+    async with base.CleanModel() as model:
+        await model.deploy(str(charm_path))
+        assert 'charm-secret' in model.applications
+        await model.wait_for_idle(status="active")
+        assert model.units['charm-secret/0'].workload_status == 'active'
+
+        secrets = await model.list_secrets(show_secrets=True)
+        assert secrets is not None
+        assert len(secrets.results) == 1
