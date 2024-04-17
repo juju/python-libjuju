fcfa17dd6ad08f4d0af13a8352da1e9ac070169e
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2024-04-17 11:20:58 +0200

    Update tests/integration/test_model.py
    Co-authored-by: Caner Derici <caner.derici@canonical.com>

--- tests/integration/test_model.py
+++ tests/integration/test_model.py
@@ -185,7 +185,7 @@ async def test_deploy_bundle_local_charm_series_manifest():
 @base.bootstrapped
 @pytest.mark.bundle
 async def test_deploy_bundle_with_pinned_charm_revision():
-    bundle_dir = INTEGRATION_TEST_DIR / 'bundle-with-charm-revision.yaml'
+    bundle_dir = INTEGRATION_TEST_DIR / 'bundle'
     bundle_yaml_path = bundle_dir / 'bundle-include-file.yaml'
     # Revision of the hello-juju charm defined in the bundle yaml
     # We can also read the yaml to get the revision but wr're hard-coding it for now for simplicity
