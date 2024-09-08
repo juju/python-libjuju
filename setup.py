ceb8c141097fc7406117b13653ea7aaf7456a9a5
Dima Tisnek <dimaqq@gmail.com>
2024-09-08 14:13:00 +0900

    py 3.11 for now

--- setup.py
+++ setup.py
@@ -48,9 +48,8 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     entry_points={
         'console_scripts': [
