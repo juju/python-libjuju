beb570a366975c005f3bd3d29b4149d737efa171
James Garner <james.garner@canonical.com>
2026-07-22 15:51:03 +1200

    docs: use rst_prolog instead of a hook for incremental builds

--- docs/conf.py
+++ docs/conf.py
@@ -305,30 +305,16 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
 
-
 def setup(app):
     app.add_css_file("custom.css")
-    app.connect("source-read", _prepend_deprecation_notice)
-
 
 # Notice prepended to every built page so that visitors arriving on any page
 # (e.g. via a search engine or a deep link) see the deprecation warning.
-# We use a custom source-read hook rather than rst_prolog to avoid showing the
-# notice twice on the landing page, as index.rst includes readme.rst.
-_DEPRECATION_NOTICE = """\
+rst_prolog = """
 .. warning::
 
    **python-libjuju is deprecated.**
 
    Charms should migrate to `Jubilant <https://github.com/canonical/jubilant>`_, Canonical's recommended library for Juju integration tests.
    See `How to migrate integration tests from pytest-operator <https://canonical.com/juju/docs/ops/latest/howto/migrate/migrate-integration-tests-from-pytest-operator/>`_.
-
 """
-
-
-def _prepend_deprecation_notice(app, docname, content):
-    if docname in ("index", "readme"):
-        return
-    # ``content`` is a list containing a single string to make in-place modification possible.
-    # https://www.sphinx-doc.org/en/master/extdev/event_callbacks.html#event-source-read
-    content[0] = _DEPRECATION_NOTICE + content[0]
